65e3e0875ae6c9f1112bd400d949fbf5ad5c1c8e
modulor <modulor@hotmail.es>
2016-12-15 11:28:25 -0600

    function changes

--- zipcodes/js/app.js
+++ zipcodes/js/app.js
@@ -9,46 +9,48 @@ var app = new Vue({
     watch: {
         fromZip: function (){
             this.fromCity = '';
-            if(this.fromZip.length == 5){
+            if(this.fromZip.length == 5)
                 this.searchZip(this.fromZip, 'fromCity');
-            }
         },
         toZip: function (){
             this.toCity = '';
-            if(this.toZip.length == 5){
+            if(this.toZip.length == 5)
                 this.searchZip(this.toZip, 'toCity');
-            }
         }
     },
     methods: {
-        searchZip: _.debounce(function(zipcode, type){
 
-            var vm = this;            
+        searchZip: function(zipcode, type){
 
+            var vm = this;
+
+            this.setCityName('Searching...', type);            
+
+            var resultado = axios.get('https://ziptasticapi.com/'+zipcode).then(function(res){               
+                    
+                if(typeof res.data.error == 'undefined'){
+                    vm.setCityName(res.data.city+ '. '+res.data.state, type);
+                }
+                else{
+                    vm.setCityName('Invalid ZIPCode',type);
+                }
+                
+            })
+            .catch(function (error){
+                vm.setCityName('Invalid ZIPCode',type);
+            });
+
+        },
+        setCityName: function(texto, type){            
             switch(type){
                 case 'fromCity':
-                    this.fromCity = 'searching... ';
-                    
-                    axios.get('https://ziptasticapi.com/'+zipcode).then(function(res){
-                        vm.fromCity = res.data.city+ ', '+res.data.state;
-                    })
-                    .catch(function (error){
-                        vm.fromCity = 'Invalid ZIPCode';
-                    });
+                    this.fromCity = texto;
                 break;
                 case 'toCity':
-                    this.toCity = 'searching... ';
-                    
-                    axios.get('https://ziptasticapi.com/'+zipcode).then(function(res){
-                        vm.toCity = res.data.city+ ', '+res.data.state;
-                    })
-                    .catch(function (error){
-                        vm.toCity = 'Invalid ZIPCode';
-                    });
+                    this.toCity = texto;
                 break;
             }
+        }
 
-
-        },500)
     }
 });
